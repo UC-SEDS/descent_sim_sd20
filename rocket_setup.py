42470f0bab09bdc84af894cfc9f401d2fce8ca38
ucsed <uc.seds@outlook.com>
2020-01-06 21:40:41 -0500

    updated sim

--- rocket_setup.py
+++ rocket_setup.py
@@ -4,8 +4,8 @@ from tools import toslugs, tofeet
 
 def rocket_setup():
     # import parachutes
-    drogue = drogue_chutes['36']
-    main = main_chutes['certXL']
+    drogue = drogue_chutes['24']
+    main = main_chutes['certXXL']
 
     # Constraints
     max_ke = 75.0  # ft lbs
@@ -15,7 +15,7 @@ def rocket_setup():
     phases = 2
     # set up mission parameters
     dt = [0.01, 0.01]  # time step for each phase
-    mass = [toslugs(31.8, 'lb'), toslugs(31.8 - 4.0, 'lb')]  # mass for each phase
+    mass = [toslugs(32.0, 'lb'), toslugs(32.0, 'lb')]  # mass for each phase
     bc = [500.0, 0.0]  # altitude breaking conditions
     chutes = [drogue, main]  # parachute used for each phase
     return [phases, initial_state, dt, bc, mass, chutes, max_time, max_ke]
